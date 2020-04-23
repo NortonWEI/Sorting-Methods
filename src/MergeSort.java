5118702ce3f3d2e22da0803cc02ffc4665adc4fa
Wenzhou Wei <wenzhouw@student.unimelb.edu.au>
2020-04-23 18:18:19 +1000

    merge sort

---
+++ src/MergeSort.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static int[] sort(int[] array) {
+        if (array == null || array.length <= 1) return array;
+
+        // divide
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+
+        // conquer
+        return mergeTwoArray(sort(left), sort(right));
+    }
+
+    public static int[] mergeTwoArray(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+
+        return result;
+    }
+}
--- src/Tester.java
+++ src/Tester.java
@@ -1,17 +1,15 @@
+import java.util.Arrays;
+
 public class Tester {
     public static void main(String[] args) {
         int[] array = {4,8,7,1,9,5,6,3,10,0,2};
         /*
-        for (int e: BubbleSort.sort(array)) System.out.printf("%d,", e);
-        System.out.println();
-        for (int e: QuickSort.sort(array, 0, array.length-1)) System.out.printf("%d,", e);
-        System.out.println();
-        for (int e: InsertionSort.sort(array)) System.out.printf("%d,", e);
-        System.out.println();
-        for (int e: ShellSort.sort(array)) System.out.printf("%d,", e);
-        System.out.println();
+        System.out.printf(Arrays.toString(BubbleSort.sort(array)));
+        System.out.println(Arrays.toString(QuickSort.sort(array, 0, array.length-1)));
+        System.out.println(Arrays.toString(InsertionSort.sort(array)));
+        System.out.println(Arrays.toString(ShellSort.sort(array)));
+        System.out.println(Arrays.toString(SelectionSort.sort(array)));
          */
-        for (int e: SelectionSort.sort(array)) System.out.printf("%d,", e);
-        System.out.println();
+        System.out.println(Arrays.toString(MergeSort.sort(array)));
     }
 }
